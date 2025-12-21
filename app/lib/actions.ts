14bfe8f6ac409a96a34590efb0ddfaf434106500
unknown <l200230183@student.ums.ac.id>
2025-12-21 10:58:37 +0700

    asd

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -127,3 +127,8 @@ export async function authenticate(
     throw error;
   }
 }
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
+}
